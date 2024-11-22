e2f298df90e09f64d43a2e887d7f3bbfcb95aaf7
Jhoandris B <jrbaronp@gmail.com>
2024-11-22 08:35:47 -0500

    model

---
+++ lib/models/note.dart
@@ -0,0 +1,15 @@
+class Note {
+  final int id;
+  final String title;
+  final String content;
+
+  Note({required this.id, required this.title, required this.content});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+    };
+  }
+}
