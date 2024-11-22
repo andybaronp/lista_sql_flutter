445d94cb56d0c0d1f955977110497f63db0224f1
Jhoandris B <jrbaronp@gmail.com>
2024-11-22 08:35:55 -0500

    operations sql

---
+++ lib/db/operation.dart
@@ -0,0 +1,40 @@
+import 'package:lista_sql/models/note.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class Operation {
+  /// Abre la base de datos, crendola si no existe.
+  Future<Database> _openDB() async {
+    return openDatabase(join(await getDatabasesPath(), 'notes.db'),
+        onCreate: (db, version) {
+      // Crea la tabla "notes" con los campos "id", "title" y "content".
+      return db.execute(
+          "CREATE TABLE notes(id INTEGER PRIMARY KEY, title TEXT, content TEXT)");
+    });
+  }
+
+  /// Inserta una nota en la base de datos.
+  Future<void> insertNote(Note note) async {
+    final db = await _openDB();
+    // Inserta la nota en la tabla "notes" reemplazando cualquier
+    // nota existente con el mismo id.
+    await db.insert('notes', note.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  /// Obtiene una lista de todas las notas en la base de datos.
+  Future<List<Note>> getNotes() async {
+    final db = await _openDB();
+    // Obtiene una lista de mapas de la tabla "notes".
+    final List<Map<String, dynamic>> maps = await db.query('notes');
+
+    // Convierte la lista de mapas en una lista de notas.
+    return List.generate(maps.length, (i) {
+      return Note(
+        id: maps[i]['id'],
+        title: maps[i]['title'],
+        content: maps[i]['content'],
+      );
+    });
+  }
+}
